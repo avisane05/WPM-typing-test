95d57b368bbf0be2b816b0aec2bd27049be2b8d9
avisane05 <avisane05.as@gmail.com>
2024-05-15 11:23:14 +0530

    code 1

--- WPM_typing_test.py
+++ WPM_typing_test.py
@@ -8,11 +8,23 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def wpm_test(stdscr):
+    targat_text = "Hello world this is some text for this app!"
+    current_text = []
+    
+    stdscr.clear()
+    stdscr.addstr(targat_text)
+    stdscr.refresh()
+
+
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
