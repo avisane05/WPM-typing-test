4ef435bc2fa7c983591dca0e97a7bdfa503b57a7
avisane05 <avisane05.as@gmail.com>
2024-05-15 12:13:27 +0530

    code 4

--- WPM_typing_test.py
+++ WPM_typing_test.py
@@ -1,5 +1,6 @@
 import curses
 from curses import wrapper
+import time
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -10,6 +11,7 @@ def start_screen(stdscr):
 
 def display_test(stdscr, targat, current, wpm=0):
     stdscr.addstr(targat)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = targat[i]
@@ -20,18 +22,30 @@ def display_test(stdscr, targat, current, wpm=0):
         stdscr.addstr(0, i, char, color)
 
 
+
 def wpm_test(stdscr):
     targat_text = "Hello world this is some text for this app!"
     current_text = []
+    wpm=0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
-    while True:        
+    while True:
+        time_escaped = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_escaped / 60)) / 5)
+
         stdscr.clear()
-        
-        display_test(stdscr, targat_text, current_text)
-
+        display_test(stdscr, targat_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        if "".join(current_text) == targat_text:
+            stdscr.nodelay(False)
+            break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
         if ord(key) == 27:
             break
@@ -50,8 +64,14 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    
+
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
 
 wrapper(main)
